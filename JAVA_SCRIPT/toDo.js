0985c8c8b00c3d312bb5e8c307d09457604555ea
74716 <ndeb2450@gmail.com>
2024-03-12 15:50:27 +0530

    instead of a item string item object is added which included date and time too delete functionality isnt working now

--- JAVA_SCRIPT/toDo.js
+++ JAVA_SCRIPT/toDo.js
@@ -1,12 +1,19 @@
 //Array to Keep List of To Do Tasks
-toDoList = ['natasha', 'jash'];
+toDoList = [];
 display_toDoList();
 function addTo_toDoList(){
     //from document we are selecting the value inside text input box having ID 'input-box'
     let toDoItem = document.querySelector('#input-box').value;
+    let toDoDate = document.querySelector('#input-date').value;
+    let toDOTime = document.querySelector('#input-time').value;
+
+    let everyListObject = {item: toDoItem, 
+                           date: toDoDate,
+                           time: toDOTime
+                          };
 
     //on every click adding it to the Array of to Do List
-    toDoList.push(toDoItem);
+    toDoList.push(everyListObject);
      console.log(toDoList);
      display_toDoList();
 }
@@ -18,12 +25,14 @@ function display_toDoList(){
         //newHTML element is being modified everytime enirely and due to for loop all elements are added again
         newHTML+= 
         `<div>
-             <span style="margin-right: 100px;">${toDoList[i]}</span>  
-             <button id="delete-button" onclick = "
+            <span style="margin-right: 100px;">${toDoList[i].item}</span> 
+             <span style="margin-right: 100px;">${toDoList[i].date}</span> 
+             <span style="margin-right: 100px;">${toDoList[i].time}</span>  
+             <button id="delete-button" onclick = 
              toDoList.splice (${i},1);
              display_toDoList();"
              >Delete</button> 
-             <br>
+             <br> 
         </div>`; 
      }
      //the div is being updated with newHTML on every Addition
